c7888eb2d64d0ad88a85999cabe55e4fc39da763
pedro akira <pedro-akira@hotmail.com>
2020-01-04 09:47:07 -0300

    add script.sh about create folders and file course oracle rac

--- Oracle_Database_12c_RAC_Administration/script.sh
+++ Oracle_Database_12c_RAC_Administration/script.sh
@@ -1,5 +1,292 @@
+
+
+
+
+cd secao1_introduction
+
+touch 1_Introduction.
+
+
+
+
+
+
+
+
+cd secao2_Oracle_RAC_Fundamentals
+
+touch 2_Oracle_RAC_Database_Overview.txt
+
+touch 3_Oracle_RAC_Architecture_Part_1_of_3.txt
+
+touch 4_Oracle_RAC_Architeture_Part_2_of_3.txt
+
+touch Oracle_RAC_Architecture_Part_3_of_3.txt
+
+
+
+
+
+
+
+cd secao3_Installing_and_Creating_Oracle_RAC_database
+
+
+touch Installing_and_Creating_Oracle_RAC.txt
+
+touch Practice_1_Preparing_Practice_Environment_Part_1_of_4.txt
+
+touch Practice_1_Preparing_Practice_Environment_Part_2_of_4.txt
+
+touch Practice_1_Preparing_Practice_Environment_Part_3_of_4.txt
+
+touch Practice_1_Preparing_Practice_Environment_Part_4_of_4.txt
+
+touch Practice_2_Create_Oracle_12c_R1_Two-Node_RAC_Database_Part_1_of_4.txt
+
+touch Practice_2_Create_Oracle_12c_R1_Two-Node_RAC_Database_Part_2_of_4.txt
+
+touch Practice_2_Create_Oracle_12c_R1_Two-Node_RAC_Database_Part_3_of_4.txt
+
+touch Practice_2_Create_Oracle_12c_R1_Two-Node_RAC_Database_Part_4_of_4.txt
+
+
+
+
+
+
+
+
+
+
+cd secao4_Administering_Oracle_RAC
+
+
+touch Oracle_RAC_Basic_Administration_Part_1_of_2.txt
+
+touch Oracle_RAC_Basic_Administration_Part_2_of_2.txt
+
+touch Practice_3_Oracle_RAC_Administration_Topics_Part_1_of_3.txt
+
+touch Practice_3_Oracle_RAC_Administration_Topics_Part_2_of_3.txt
+
+touch Practice_3_Oracle_RAC_Administration_Topics_Part_3_of_3.txt
+
+touch Managing_Backup_and_Recovery_in_Oracle_RAC.txt
+
+touch Practice_4_Managing_Backup_and_Recovery_in_Oracle_RAC.txt
+
+touch Practice_5_Installing_and_Using_Swingbench_2.5.txt
+
+touch Practice_6_Getting_Familiar_with_Oracle_EM_Database_Express.txt
+
+
+
+
+
+
+
+
+cd secao5_Monitoring_and_Tuning_Oracle_RAC_Database
+
+touch Global_Resource_Management_in_Oracle_RAC.txt
+
+touch Monitoring_and_Tuning_Oracle_RAC_Database_-_Part_1_of_2.txt
+
+touch Monitoring_and_Tuning_Oracle_RAC_Database_-_Part_2_of_2.txt
+
+touch Practice_7_Monitoring_and_Tuning_Oracle_RAC_Database_Part_1_of_3.txt
+
+touch Practice_7_Monitoring_and_Tuning_Oracle_RAC_Database_-_Part_2_of_3.txt
+
+touch Practice_7_Monitoring_and_Tuning_Oracle_RAC_Database_-_Part_3_of_3.txt
+
+
+
+
+
+
+
+
+cd secao6_Enabling_Oracle_RAC_High_Avaliability_Features
+
+touch Managing_Dynamic_Database_Services.txt
+
+touch Practice_8_Managing_Services_in_Oracle_RAC.txt
+
+touch Implementing_Connection_Load_Balancing_and_TAF_-_Part_1_of_2.txt
+
+
+touch Implementing_Connection_Load_Balancing_and_TAF_-_Part_2_of_2.txt
+
+touch Practice_9_Implementing_Connection_Load_Balancing_and_TAF_-_Part_1_of_3.txt
+
+
+touch Practice_9_Implementing_Connection_Load_Balancing_and_TAF_-_Part_2_of_3.txt
+
+touch Practice_9_Implementing_Connection_Load_Balancing_and_TAF_-_Part_3_of_3.txt
+
+touch Using_Application_Continuity.txt
+
+touch Practice_10_Using_Application_Continuity.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cd secao7_Applying_Patches_and_Upgrading_RAC
+
+touch Patching_Oracle_RAC_-_Part_1_of_2.txt
+
+touch Patching_Oracle_RAC_-_Part_2_of_2.txt
+
+touch Practice_11_Applying_Patch_Set_Update_PSU_Part_1_of_2.txt
+
+
+touch Practice_11_Applying_Patch_Set_Update_PSU_Part_2_of_2.txt
+
+touch Upgrading_Oracle_RAC.txt
+
+touch Practice_12_Upgrading_Oracle_RAC_Database_Part_1_of_4.txt
+
+
+touch Practice_12_Upgrading_Oracle_RAC_Database_Part_2_of_4.txt
+
+
+touch Practice_12_Upgrading_Oracle_RAC_Database_Part_3_of_4.txt
+
+
+touch Practice_12_Upgrading_Oracle_RAC_Database_Part_4_of_4.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cd secao8_Managing_Oracle_RAC_One_Node
+
+touch Oracle_RAC_One_Node.txt
+
+touch Practice_13_Creating_a_RAC_One_Node_Database.txt 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cd secao9_Oracle_Multitenant_Architecture_and_RAC
+
+
+touch Multitenant_Architecture_and_RAC.txt
+
+touch Practice_14_Creating_a_Multitenant_RAC_Database_Part_1_of_2.txt
+
+touch Practice_14_Creating_a_Multitenant_RAC_Database_Part_2_of_2.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cd secao10_Managing_Policy_managed_databases
+
+touch Managing_Policy-managed_Oracle_RAC_Databases-Part_I.txt
+
+touch Practice_15_Creating_Policy-Managed_Oracle_RAC_Database.txt
+
+touch Managing_Policy-Managed_Oracle_RAC_Databases_Part_II-Part_1_of_2.txt
+
+touch Managing_Policy-Managed_Oracle_RAC_Databases_Part_II-Part_2_of_2.txt
+
+touch Practice_16_Using_Server_Categorization_and_Cluster_Configuration_Policies.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 cd secao11_Oracle_RAC_Flex_Cluster
 
 
 touch Oracle_Flex_Clusters.txt 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cd secao12_Appendices
+
+touch Creating_an_Oracle_12c_R2_Physical_Standby_RAC_database_from_a_Primary_RAC.txt
 
